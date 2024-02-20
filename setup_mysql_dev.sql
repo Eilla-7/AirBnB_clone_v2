24a5db11a2909f588a2d6cf5beebee17766383bd
Eilla-7 <eilafadel2238@gmail.com>
2024-02-21 01:08:01 +0300

    Database For The Project

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,5 @@
--- Create or replace the database hbnb_dev_db
+--  Prepares a MySQL server for the project with database named hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- Create or replace the user hbnb_dev with password hbnb_dev_pwd
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
--- Grant all privileges on hbnb_dev_db to user hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
--- Grant SELECT privilege on performance_schema to user hbnb_dev
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
