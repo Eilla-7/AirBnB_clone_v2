9ca931f702eb1d14a0a078f6c202f61da00341cf
MemanAwad <meman1awad@gmail.com>
2024-03-24 19:49:58 +0200

    add function that accept only integers values

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+""" simple flask program"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """ function to diplay hello HBNB"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def fun_HBNB():
+    """ function to diplay HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_HBNB(text):
+    """ function to diplay C <text>"""
+    text = text.replace('_', ' ')
+    return f'C {text}'
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def py_HBNB(text):
+    """ function to diplay python <text>"""
+    text = text.replace('_', ' ')
+    return f'Python {text}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number_HBNB(n):
+    """ function to diplay the number passed"""
+    return f'{n} is a number'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
